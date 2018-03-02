038b2e8cdf58c186a885b6840c3f55cd69ac56ab
dhannyell <dhannyelllukas@hotmail.com>
2018-03-02 02:08:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
+setup(
+    name='exemplo',
+    install_requires=[
+        'telegram',
+        'python-telegram-bot',
+        'redis',
+        'BeautifulSoup4'
+    ],
+)
+
 from main import updater
 updater.start_polling()
