afb716386662912ce2f41a0690a2372459813d75
dhannyell <dhannyelllukas@hotmail.com>
2018-03-02 02:33:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
+import os
+print "Trying to Install required module: telegram\n"
+os.system('pip3 install telegram')
+os.system('pip3 install python-telegram-bot')
+os.system('pip3 install redis')
+os.system('pip3 install BeautifulSoup4')
+
+from main import updater
+updater.start_polling()
+
+
 from main import updater
 updater.start_polling()
