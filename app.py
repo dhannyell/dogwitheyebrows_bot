65d244bb8529241ded999aa6729a2bc267ab340b
dhannyell <dhannyelllukas@hotmail.com>
2018-03-02 02:40:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ os.system('pip3 install telegram')
 os.system('pip3 install python-telegram-bot')
 os.system('pip3 install redis')
 os.system('pip3 install BeautifulSoup4')
+os.system('pip3 install requests')
 
 from main import updater
 updater.start_polling()
